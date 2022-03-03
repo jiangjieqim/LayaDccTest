db3fd903a840bd6ddc17c03361f31a69e5f8a1aa
jiangjieqim <jiangjieqim@hotmail.com>
2022-03-03 16:22:40 +0800

    修改脚本

--- layah5/生成cache.bat
+++ layah5/生成cache.bat
@@ -1,2 +1,11 @@
-cd bin
-layadcc d:/github/LayaDccTest/layah5/bin -cache -url https://test1.webgame.zhaouc.com/fq4_hulu/index_native.html
+:: 1.将资源文件放置ddcfold中
+:: 2.ddcfold结构为ddcfold/assets,ddcfold/libs 
+:: 3.执行脚本,生成文件到d:/github/LayaDccTest/layah5/ddcOut
+
+
+
+layadcc d:/github/LayaDccTest/layah5/ddcfold -cache -url https://test1.webgame.zhaouc.com/tr2_hulu/index_native.html -cout d:/github/LayaDccTest/layah5/ddcOut
+
+
+
+pause
