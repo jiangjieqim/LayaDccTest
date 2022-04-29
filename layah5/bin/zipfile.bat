3b7514d003393eff9dcb3a2bb325e5a48ada8b30
jiangjieqim <jiangjieqim@hotmail.com>
2022-04-29 21:40:05 +0800

    修改zipjs路径为绝对路径

--- layah5/bin/zipfile.bat
+++ layah5/bin/zipfile.bat
@@ -1,7 +1,6 @@
 echo off
+set NODE_PATH=D:/github/nodelib/
 set PROJECT=D:/github/LayaDccTest/layah5/bin/
 d:
-cd ..\..\..\nodelib
-node zipjs.js -i %PROJECT%main2.min/main2.min.js -o %PROJECT%main2.min.jar
-node zipjs.js -i %PROJECT%main.min/main.min.js -o %PROJECT%main.min.jar
-
+node %NODE_PATH%zipjs.js -i %PROJECT%main2.min/main2.min.js -o %PROJECT%main2.min.jar
+node %NODE_PATH%zipjs.js -i %PROJECT%main.min/main.min.js -o %PROJECT%main.min.jar
