dacbf89ae384301fb9b6ccf7b6dbed5c1f4773b4
jiangjieqim <jiangjieqim@hotmail.com>
2022-04-29 15:01:20 +0800

    使用范例 node d:\github\nodelib\myzipjs.js -i "D:\github\LayaDccTest\layah5\bin\main.min\main.min.js" -o "D:\github\LayaDccTest\layah5\bin\main.min.jar"

--- layah5/bin/zipfile.bat
+++ layah5/bin/zipfile.bat
@@ -2,6 +2,6 @@ echo off
 set PROJECT=D:/github/LayaDccTest/layah5/bin/
 d:
 cd ..\..\..\nodelib
-node zipjs.js %PROJECT%main2.min/main2.min.js %PROJECT%main2.min.jar
-node zipjs.js %PROJECT%main.min/main.min.js %PROJECT%main.min.jar
+node zipjs.js -i %PROJECT%main2.min/main2.min.js -o %PROJECT%main2.min.jar
+node zipjs.js -i %PROJECT%main.min/main.min.js -o %PROJECT%main.min.jar
 
